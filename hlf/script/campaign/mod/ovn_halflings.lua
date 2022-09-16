cfc970d6fe27cdd3c35f2710f440875c770c0f61
Tibor Ilić <tilic5@gmail.com>
2022-09-16 22:09:24 +0200

    hlf cooking and restaurants tweaks

--- hlf/script/campaign/mod/ovn_halflings.lua
+++ hlf/script/campaign/mod/ovn_halflings.lua
@@ -106,8 +106,116 @@ local function new_game_startup()
     end, 0)
 end
 
+local function on_every_first_tick()
+    local moot_string = "ovn_hlf_the_moot"
+	local moot = cm:get_faction(moot_string)
+    cm:set_faction_max_secondary_cooking_ingredients(moot, 2)
+
+    if cm:get_local_faction(true):name() == "ovn_hlf_the_moot" then
+        core:remove_listener('ovn_hlf_on_opened_settlement_panel')
+        core:add_listener(
+            'ovn_hlf_on_opened_settlement_panel',
+            'PanelOpenedCampaign',
+            true,
+            function(context)
+                if context.string ~= "settlement_panel" then return end
+    
+                real_timer.unregister("ovn_hlf_show_restaurant_slots_real_timer")
+                real_timer.register_repeating("ovn_hlf_show_restaurant_slots_real_timer", 0)
+            end,
+            true
+        )
+    
+        core:remove_listener("ovn_hlf_show_restaurant_slots_real_timer")
+        core:add_listener(
+            "ovn_hlf_show_restaurant_slots_real_timer",
+            "RealTimeTrigger",
+            function(context)
+                    return context.string == "ovn_hlf_show_restaurant_slots_real_timer"
+            end,
+            function()
+                local ui_root = core:get_ui_root()
+                local settlement_list = find_uicomponent(ui_root, "settlement_panel", "settlement_list")
+                if not settlement_list then
+                    real_timer.unregister("ovn_hlf_show_restaurant_slots_real_timer")
+                    return
+                    end
+            
+                for i=0, settlement_list:ChildCount()-1 do
+                    local comp = settlement_list:Find(i) and UIComponent(settlement_list:Find(i))
+                    if comp then
+                        local button_player_foreign_view = find_uicomponent(comp, "settlement_view", "toggle_button_holder", "button_list", "button_player_foreign_view")
+                        if button_player_foreign_view:Visible() then
+                            local daemon = find_uicomponent(comp, "settlement_view", "hostile_views", "wh3_daemon_factions")
+                            if daemon then
+                                local daemon_slots = find_uicomponent(daemon, "slots")
+                                if daemon_slots then
+                                    daemon:SetVisible(true)
+                                end
+                            end
+                        end
+                    end
+                end
+            end,
+            true
+        )
+    end
+end
+
+core:remove_listener('ovn_hlf_on_opened_cooking_panel')
+core:add_listener(
+	'ovn_hlf_on_opened_cooking_panel',
+	'PanelOpenedCampaign',
+	true,
+	function(context)
+		if cm:get_local_faction(true):name() ~= "ovn_hlf_the_moot" then return end
+
+		local ui_root = core:get_ui_root()
+		local cauldron = find_uicomponent(ui_root, "groms_cauldron")
+		if not cauldron then return end
+
+		local cauldron_bg = find_uicomponent(cauldron, "animated_background")
+		cauldron_bg:SetImagePath("ui/skins/default/hlfng_burrow_panel_background.png")
+
+		local arch = find_uicomponent(cauldron, "mid_colum", "pot_holder", "arch")
+		local pot = find_uicomponent(cauldron, "mid_colum", "pot_holder", "pot")
+		arch:SetVisible(false)
+		pot:SetVisible(false)
+
+		local startingX = nil
+		local startingY = nil
+		local ingredients_and_effects = find_uicomponent(cauldron, "mid_colum", "pot_holder", "ingredients_and_effects")
+		for i=1,4 do
+			local slot = find_uicomponent(ingredients_and_effects, "main_ingredient_slot_"..i)
+			if slot then
+				if not startingX then
+					startingX, startingY = slot:Position()
+				end
+
+				local currentX, currentY = slot:Position()
+
+				local deltaY = 300
+				if i==2 or i==3 then
+					deltaY = 300
+				end
+				slot:MoveTo(currentX, startingY+deltaY)
+
+				if i==1 or i==4 then
+					slot:SetCanResizeHeight(true)
+					slot:SetCanResizeWidth(true)
+					slot:Resize(64,64)
+					slot:ResizeCurrentStateImage(0,64,64)
+				end
+			end
+		end
+	end,
+	true
+)
+
 cm:add_first_tick_callback(
 	function()
+        on_every_first_tick()
+
 		if cm:is_new_game() then
 			if cm:get_campaign_name() == "main_warhammer" then
 				local ok, err =
