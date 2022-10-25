9508875f03aba52bf18416490d8ae102e6407a40
Tibor Ilić <tilic5@gmail.com>
2022-10-25 21:39:25 +0200

    fimir slave stuff

--- fimir/script/campaign/mod/ovn_fimir.lua
+++ fimir/script/campaign/mod/ovn_fimir.lua
@@ -1,5 +1,122 @@
 local function on_every_first_tick()
+        core:remove_listener("ovn_fimir_remove_fog_vanguard")
+        core:add_listener(
+            "ovn_fimir_remove_fog_vanguard",
+            "BattleCompleted",
+            true,
+            function(context)
+                if not cm:pending_battle_cache_human_is_involved() then
+                    return
+                end
+
+                for i = 1, cm:pending_battle_cache_num_defenders() do
+                    local this_char_cqi = cm:pending_battle_cache_get_defender(i);
+                    local char = cm:get_character_by_cqi(this_char_cqi)
+                    if char:faction():subculture()=="ovn_sc_fim_fimir" then
+                        cm:remove_effect_bundle_from_characters_force("ovn_fimir_fog_diktat_empty", char:cqi())
+                    end
+                end
+                for i = 1, cm:pending_battle_cache_num_attackers() do
+                    local this_char_cqi = cm:pending_battle_cache_get_attacker(i);
+                    local char = cm:get_character_by_cqi(this_char_cqi)
+                    if char:faction():subculture()=="ovn_sc_fim_fimir" then
+                        cm:remove_effect_bundle_from_characters_force("ovn_fimir_fog_diktat_empty", char:cqi())
+                    end
+                end
+            end,
+            true
+        )
+
+        core:remove_listener("ovn_fimir_give_fog_vanguard")
+        core:add_listener(
+            "ovn_fimir_give_fog_vanguard",
+            "PendingBattle",
+            function()
+                return true
+            end,
+            function(context)
+                if not cm:pending_battle_cache_human_is_involved() then
+                    return
+                end
+
+                local first_attacker_cqi = cm:pending_battle_cache_get_attacker(1);
+                local first_attacker_char = cm:get_character_by_cqi(first_attacker_cqi)
+
+                local any_region_has_fimir_fog = false
+                for _, region in model_pairs(first_attacker_char:region():province():regions()) do
+                    if region:has_effect_bundle("ovn_fimir_fog_diktat") then
+                        any_region_has_fimir_fog = true
+                        break
+                    end
+                end
+
+                if not any_region_has_fimir_fog then return end
+
+                for i = 1, cm:pending_battle_cache_num_defenders() do
+                    local this_char_cqi = cm:pending_battle_cache_get_defender(i);
+                    local char = cm:get_character_by_cqi(this_char_cqi)
+                    if char:faction():subculture()=="ovn_sc_fim_fimir" then
+                        local custom_bundle = cm:create_new_custom_effect_bundle("ovn_fimir_fog_diktat_empty")
+                        custom_bundle:add_effect("wh_main_effect_attribute_enable_vanguard_deployment", "character_to_force_own", 1)
+                        cm:apply_custom_effect_bundle_to_characters_force(custom_bundle, char)
+                    end
+                end
+                for i = 1, cm:pending_battle_cache_num_attackers() do
+                    local this_char_cqi = cm:pending_battle_cache_get_attacker(i);
+                    local char = cm:get_character_by_cqi(this_char_cqi)
+                    if char:faction():subculture()=="ovn_sc_fim_fimir" then
+                        local custom_bundle = cm:create_new_custom_effect_bundle("ovn_fimir_fog_diktat_empty")
+                        custom_bundle:add_effect("wh_main_effect_attribute_enable_vanguard_deployment", "character_to_force_own", 1)
+                        cm:apply_custom_effect_bundle_to_characters_force(custom_bundle, char)
+                    end
+                end
+            end,
+            true
+        )
+
+        core:remove_listener("ovn_fimir_give_fog_bundle_to_chars")
+        core:add_listener(
+            "ovn_fimir_give_fog_bundle_to_chars",
+            "CharacterTurnStart",
+            true,
+            function(context)
+                ---@type CA_CHAR
+                local char = context:character()
+                if not cm:char_is_general_with_army(char) or not char:has_region() then return end
+
+                local any_region_has_fimir_fog = false
+                for _, region in model_pairs(char:region():province():regions()) do
+                    if region:has_effect_bundle("ovn_fimir_fog_diktat") then
+                        any_region_has_fimir_fog = true
+                        break
+                    end
+                end
+
+                if not any_region_has_fimir_fog then
+                    cm:remove_effect_bundle_from_characters_force("ovn_fimir_fog_diktat_empty", char:cqi())
+                    return
+                end
+
+                if char:faction():subculture() == "ovn_sc_fim_fimir" then
+                    local custom_bundle = cm:create_new_custom_effect_bundle("ovn_fimir_fog_diktat_empty")
+                    custom_bundle:add_effect("wh_main_effect_force_all_campaign_movement_range", "force_to_force_own_lords_army", 20)
+                    custom_bundle:add_effect("wh_main_effect_force_army_campaign_ambush_attack_success_chance", "force_to_force_own", 20)
+                    cm:apply_custom_effect_bundle_to_characters_force(custom_bundle, char)
+                else
+                    local custom_bundle = cm:create_new_custom_effect_bundle("ovn_fimir_fog_diktat_empty")
+                    custom_bundle:add_effect("wh_main_effect_force_all_campaign_movement_range", "force_to_force_own_lords_army", -50)
+                    cm:apply_custom_effect_bundle_to_characters_force(custom_bundle, char)
+                end
+            end,
+            true
+        )
+
     if cm:get_local_faction(true):subculture() == "ovn_sc_fim_fimir" then
+        local res_bar = find_uicomponent(core:get_ui_root(), "hud_campaign", "resources_bar_holder", "resources_bar")
+        if res_bar then
+            UIComponent(res_bar:CreateComponent("ovn_fimir_slaves", "ui/ovn_fimir_slaves"))
+        end
+
         core:remove_listener('ovn_fimir_on_opened_settlement_panel')
         core:add_listener(
             'ovn_fimir_on_opened_settlement_panel',
