ef5cda2f6b08a37475519b864aa9a6e94bdc7225
Tibor Ilić <tilic5@gmail.com>
2022-11-10 17:03:37 +0100

    kroll god bonuses mechanic

--- fimir/script/campaign/mod/ovn_fimir_kroll.lua
+++ fimir/script/campaign/mod/ovn_fimir_kroll.lua
@@ -96,23 +96,158 @@ local function new_game_startup()
     end, 0)
 end
 
+local get_num_temples = function(faction)
+    local num_temples = 0
+
+    ---@type CA_REGION
+    for _, region in model_pairs(faction:region_list()) do
+        ---@type CA_SLOT
+        for _, slot in model_pairs(region:slot_list()) do
+            if slot and slot:has_building() then
+                local building_key = slot:building():name()
+                if building_key == "ovn_fimir_kroll_temple" then
+                    num_temples = num_temples + 1
+                end
+            end
+        end
+    end
+
+    return num_temples
+end
+
+local give_kroll_god_bonuses = function(faction)
+    local num_temples = get_num_temples(faction)
+
+    local army_bundle_name = "ovn_fimir_kroll_bundle_1"
+    local faction_bundle_name = "ovn_fimir_kroll_bundle_faction_1"
+
+    cm:remove_effect_bundle("ovn_fimir_kroll_bundle_faction_1", faction:name())
+    cm:remove_effect_bundle("ovn_fimir_kroll_bundle_faction_2", faction:name())
+    cm:remove_effect_bundle("ovn_fimir_kroll_bundle_faction_3", faction:name())
+    cm:remove_effect_bundle("ovn_fimir_kroll_bundle_faction_4", faction:name())
+
+    if num_temples > 29 then
+        army_bundle_name = "ovn_fimir_kroll_bundle_4"
+        faction_bundle_name = "ovn_fimir_kroll_bundle_faction_4"
+    elseif num_temples > 19 then
+        army_bundle_name = "ovn_fimir_kroll_bundle_3"
+        faction_bundle_name = "ovn_fimir_kroll_bundle_faction_3"
+    elseif num_temples > 9 then
+        army_bundle_name = "ovn_fimir_kroll_bundle_2"
+        faction_bundle_name = "ovn_fimir_kroll_bundle_faction_2"
+    end
+
+    local army_custom_bundle = cm:create_new_custom_effect_bundle(army_bundle_name)
+    local faction_custom_bundle = cm:create_new_custom_effect_bundle(faction_bundle_name)
+
+    faction_custom_bundle:add_effect("ovn_fimir_kroll_army_bonuses_dummy", "faction_to_faction_own_unseen", 1)
+
+    if num_temples > 29 then
+        army_custom_bundle:add_effect("wh_main_effect_force_stat_leadership", "character_to_force_own", 8)
+        army_custom_bundle:add_effect("wh_main_effect_force_stat_weapon_strength", "character_to_force_own", 15)
+        army_custom_bundle:add_effect("ovn_fimir_kroll_dmg_reflect_enable", "character_to_force_own", 1)
+
+        faction_custom_bundle:add_effect("ovn_fimir_diplomacy_bonus_all_fimir", "faction_to_faction_own_unseen", 40)
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_4", "faction_to_faction_own_unseen", 1)
+    elseif num_temples > 19 then
+        army_custom_bundle:add_effect("wh_main_effect_force_stat_leadership", "character_to_force_own", 6)
+        army_custom_bundle:add_effect("wh_main_effect_force_stat_weapon_strength", "character_to_force_own", 10)
+
+        faction_custom_bundle:add_effect("ovn_fimir_diplomacy_bonus_all_fimir", "faction_to_faction_own_unseen", 20)
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_3", "faction_to_faction_own_unseen", 1)
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_4", "faction_to_faction_own_unseen", -1)
+    elseif num_temples > 9 then
+        army_custom_bundle:add_effect("wh_main_effect_force_stat_leadership", "character_to_force_own", 4)
+        army_custom_bundle:add_effect("wh_main_effect_force_stat_weapon_strength", "character_to_force_own", 5)
+        
+        faction_custom_bundle:add_effect("ovn_fimir_diplomacy_bonus_all_fimir", "faction_to_faction_own_unseen", 10)
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_2", "faction_to_faction_own_unseen", 1)
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_3", "faction_to_faction_own_unseen", -1)
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_4", "faction_to_faction_own_unseen", -1)
+    else
+        army_custom_bundle:add_effect("wh_main_effect_force_stat_leadership", "character_to_force_own", 2)
+
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_1", "faction_to_faction_own_unseen", 1)
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_2", "faction_to_faction_own_unseen", -1)
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_3", "faction_to_faction_own_unseen", -1)
+        faction_custom_bundle:add_effect("ovn_fimir_kroll_god_dummy_4", "faction_to_faction_own_unseen", -1)
+    end
+
+    cm:apply_custom_effect_bundle_to_faction(faction_custom_bundle, faction)
+
+    ---@type CA_CHAR
+    local char = faction:faction_leader()
+    if char:has_military_force() then
+        cm:remove_effect_bundle_from_character("ovn_fimir_kroll_bundle_1", char)
+        cm:remove_effect_bundle_from_character("ovn_fimir_kroll_bundle_2", char)
+        cm:remove_effect_bundle_from_character("ovn_fimir_kroll_bundle_3", char)
+        cm:remove_effect_bundle_from_character("ovn_fimir_kroll_bundle_4", char)
+
+        cm:apply_custom_effect_bundle_to_character(army_custom_bundle, char)
+    end
+end
+
+local function on_every_first_tick()
+    local kroll_faction = cm:get_faction("ovn_fim_tendrils_of_doom")
+    if kroll_faction and kroll_faction:is_human() then
+        give_kroll_god_bonuses(kroll_faction)
+    end
+
+    core:remove_listener("ovn_fimir_kroll_god_bonuses_on_kroll_selected")
+    core:add_listener(
+        "ovn_fimir_kroll_god_bonuses_on_kroll_selected",
+        "CharacterSelected",
+        function(context)
+            ---@type CA_CHAR
+            local char = context:character()
+            return char:character_subtype_key() == "fim_daemon_octopus_kroll" and char:faction():is_human()
+        end,
+        function(context)
+            local char = context:character()
+            give_kroll_god_bonuses(char:faction())
+        end,
+        true
+    )
+    
+    core:remove_listener("ovn_fimir_kroll_god_bonuses_on_turn_start")
+    core:add_listener(
+        "ovn_fimir_kroll_god_bonuses_on_turn_start",
+        "FactionTurnStart",
+        function(context)
+            local faction = context:faction()
+            return faction:name() == "ovn_fim_tendrils_of_doom"
+        end,
+        function(context)
+            local faction = context:faction()
+            give_kroll_god_bonuses(faction)
+        end,
+        true
+    )    
+end
 
 
 cm:add_first_tick_callback(
 	function()
-        mixer_set_faction_trait("ovn_fim_tendrils_of_doom", "ovn_tendrils_doom_faction_trait", true)
-		if cm:is_new_game() then
-			if cm:get_campaign_name() == "main_warhammer" then
-				local ok, err =
-					pcall(
-					function()
-						new_game_startup()
-					end
-				)
-				if not ok then
-					script_error(err)
-				end
-			end
-		end
+        cm:callback(function()
+            pcall(function()
+                mixer_set_faction_trait("ovn_fim_tendrils_of_doom", "ovn_tendrils_doom_faction_trait", true)
+            end)
+
+            on_every_first_tick()
+
+            if cm:is_new_game() then
+                if cm:get_campaign_name() == "main_warhammer" then
+                    local ok, err =
+                        pcall(
+                        function()
+                            new_game_startup()
+                        end
+                    )
+                    if not ok then
+                        script_error(err)
+                    end
+                end
+            end
+        end, 3)
 	end
 )
