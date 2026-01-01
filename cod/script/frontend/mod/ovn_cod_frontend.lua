bb0a8a28b71eb2091b3d5690f4941c4832f99325
Tibor Ilić <tilic5@gmail.com>
2026-01-01 17:12:28 +0100

    sync

--- cod/script/frontend/mod/ovn_cod_frontend.lua
+++
@@ -1,7 +0,0 @@
-core:add_ui_created_callback(
-    function(context)
-        mixer_add_starting_unit_list_for_faction("wh2_main_hef_citadel_of_dusk", {"wh2_dlc10_hef_inf_the_storm_riders_ror_0", "wh2_main_hef_inf_swordmasters_of_hoeth_0", "wh2_main_hef_art_eagle_claw_bolt_thrower"})
-		mixer_change_lord_name("1037111167", "ovn_stormrider")
-		 mixer_add_faction_to_major_faction_list("wh2_main_hef_citadel_of_dusk")
-    end
-)
